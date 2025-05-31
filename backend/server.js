e13224c4972d35a9af7029b65aa52f7332d3c914
Swetha <swethaannadhurai@gmail.com>
2025-05-31 13:54:50 +0530

    j

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,33 @@ app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.FRONTEND_DEPLOY_URL
+];
+
 app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "PUT", "POST", "DELETE"]
+  })
+);
+
+
+/*app.use(
 	cors({
 		origin: "https://zingy-dolphin-f24425.netlify.app",
 		methods: ["GET", "PUT", "POST", "DELETE"],
 		credentials: true,
 	})
-);
+);*/
 
 
 app.use("/api/users", require("./routes/userRoutes"));
