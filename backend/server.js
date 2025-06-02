b7ca2578d4830a1367382dbb87246226554e0d04
Swetha <swethaannadhurai@gmail.com>
2025-06-02 10:55:04 +0530

    m

--- backend/server.js
+++ backend/server.js
@@ -3,36 +3,12 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
 const path = require("path");
+const cookieParser = require('cookie-parser');
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json());
-
-
-app.use('/uploads', express.static('uploads'));
-
-
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  process.env.FRONTEND_DEPLOY_URL
-];
-
-/*app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "PUT", "POST", "DELETE"]
-  })
-);*/
-
 
 app.use(cors({
   origin: "https://zingy-dolphin-f24425.netlify.app", 
@@ -40,6 +16,9 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
+app.use('/uploads', express.static('uploads'));
+app.use(cookieParser());
+app.use(express.json());
 
 
 app.use("/api/users", require("./routes/userRoutes"));
