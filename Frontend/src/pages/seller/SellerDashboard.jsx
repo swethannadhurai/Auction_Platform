b6d364a2066220a8b11e8ec8c96c0601c604b82c
Swetha <swethaannadhurai@gmail.com>
2025-06-05 20:12:33 +0530

    zy

--- Frontend/src/pages/seller/SellerDashboard.jsx
+++ Frontend/src/pages/seller/SellerDashboard.jsx
@@ -1,21 +1,9 @@
-import { Outlet } from "react-router-dom";
-import SellerSidebar from "../../components/SellerSidebar";
-import { useEffect, useState } from "react";
-import axios from "axios";
-
+ import { Outlet } from 'react-router-dom';
+import SellerSidebar from '../../components/SellerSidebar';
 function SellerDashboard() {
-  const [profile, setProfile] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get("/api/sellers/profile", { withCredentials: true })
-      .then((res) => setProfile(res.data))
-      .catch((err) => console.error("Failed to fetch profile:", err));
-  }, []);
-
   return (
     <div className="flex">
-      <SellerSidebar profile={profile} />
+      <SellerSidebar />
       <main className="flex-1 p-6">
         <Outlet />
       </main>
@@ -24,3 +12,6 @@ function SellerDashboard() {
 }
 
 export default SellerDashboard;
+
+
+
